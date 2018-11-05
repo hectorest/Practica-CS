09b35e213a529147f974ab4c8a45aa82d96108bd
Sawn39 <33158086+Sawn39@users.noreply.github.com>
2018-11-05 13:23:18 +0100

    AES mas mejor

--- AES.java
+++ AES.java
@@ -1,4 +1,6 @@
 import javax.crypto.*;
+import javax.crypto.spec.*;
+
 
 public class AES {
 
@@ -9,10 +11,10 @@ public class AES {
 	
 	// METODO CONSTRUCTOR
 	
-	public AES() {
+	public AES() throws Exception {
 		
 		// Instanciamos un Generador de llaves en tipo AES
-		kgen = KeyGenerator.getInstance("AES");
+		KeyGenerator kgen = KeyGenerator.getInstance("AES");
 		// Inicializamos el generador especificandole un tamanyo de 128 bytes
 		kgen.init(128);
 		// Generamos un objeto de cifrado de tipo AES
@@ -22,19 +24,21 @@ public class AES {
 		// Codificamos dicha llave en bytes
 		byte[] crudo = llave.getEncoded();
 		// Construimos una clave secreta especificando que es de tipo AES
-		sKeySpec = new SecretKeySpec(crudo, "AES");//Es necesario? Se podria especificar antes
+		sKeySpec = new SecretKeySpec(crudo, "AES");
 	}
+	
+	
 	
 	/**
 	 * Emplearemos una funcion que convierte un array de bytes en una cadena
-	 * dicha funcion la utilizaremos para desplegar el contenido de lo
-	 * encriptado en consola
+	 * dicha funcion la utilizaremos para desplegar el contenido del
+	 * criptosistema
 	 * 
 	 * @param  array de bytes a convertir en String
 	 * @return  El String
 	 */
 	
-	public static String asHex(byte buf[]) {
+	public String asHex(byte buf[]) {
 		StringBuffer strbuf = new StringBuffer(buf.length * 2);
 		for(int i=0; i<buf.length; i++) {
 			if(((int) buf[i] & 0xff) < 0x10) {
@@ -45,6 +49,8 @@ public class AES {
 		return strbuf.toString();
 	}
 
+	
+	
 	/**
 	 * Encriptamos el archivo pasado por parametro
 	 * 
@@ -53,7 +59,7 @@ public class AES {
 	 * @return mensaje encriptado en String
 	 */
 	
-	public String encriptarArchivo (String s) {
+	public String encriptarArchivo (String s) throws Exception {
 		
 		// Inicializamos el sistema de ahora en modo de ENCRIPTACION con la clave del constructor
 		cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);
@@ -63,6 +69,8 @@ public class AES {
 		return asHex(encriptado);
 	}
 	
+	
+	
 	/**
 	 * Desencriptamos el archivo pasado por parametro
 	 * 
@@ -71,14 +79,29 @@ public class AES {
 	 * @return mensaje en claro en String
 	 */
 	
-	public String decriptarArchivo(byte[] c) {
+	public String decriptarArchivo(byte[] c) throws Exception {
+		byte[] desencriptado = null;
 		
 		// Inicializamos el sistema de ahora en modo de DESENCRIPTACION con la clave del constructor
 		cipher.init(Cipher.DECRYPT_MODE, sKeySpec);
 		// Obtenemos el array de bytes de lo decriptado
-		byte[] desencriptado = cipher.doFinal(c);
+		desencriptado = cipher.doFinal(c);
 		
-		return asHex(desencriptado);
+		return (new String(desencriptado));
+	}
+	
+	
+	
+	/**
+	 * Getter de la clave privada
+	 * 
+	 * @throws Exception
+	 * @return skeySpec
+	 */
+	
+	public String getClavePrivada() {
+		byte[] llave = sKeySpec.getEncoded();
+		return (asHex(llave));
 	}
 
 }
