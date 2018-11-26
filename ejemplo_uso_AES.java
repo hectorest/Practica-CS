90bb37dc41487dacc1e00f320abe309ed917200a
hectorest <33354984+hectorest@users.noreply.github.com>
2018-11-26 13:36:34 +0100

    EJEMPLO USO AES

--- ejemplo_uso_AES.java
+++ ejemplo_uso_AES.java
@@ -1,18 +1,20 @@
-import javax.crypto.*;
-import javax.crypto.spec.*;
-import AES.java;
+package pruebas;
 
-public class ejemploUso {
-	
+public class ejemplo_uso_aes {
+
 	public static void main(String[] args) throws Exception {
 		
-		if(args[]!=null && args.length==1) {
+		if(args!=null && args.length==1) {
 			System.out.println("El string original es: " + args[0]);
-			String encriptado = AES.encriptarArchivo(args[0]);
+			
+			aes aes1 = new AES();
+			
+			String encriptado = aes1.encriptarArchivo(args[0]);
 			System.out.println("El string encriptado es: " + encriptado);
-			String desencriptado = AES.desencriptarArchivo(encriptado);
+			byte[] encriptadoBytes = encriptado.getBytes();
+			String desencriptado = aes1.desencriptarArchivo(encriptado);
 			System.out.println("El string desencriptado es: " + desencriptado);
 		}
 	}
-
+	
 }
